3554bfc4d080ac65f79f8e5dcafd076342853d8a
Afara Seun <seunvigo@gmail.com>
2021-03-24 19:56:11 +0100

    css ehn :-D

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,28 +1,28 @@
 <template>
-  <div
-    class="as-container as-logo-container flex items-center px-4 py-3 fixed left-0 right-0 top-0 z-10"
-  >
-    <div class="as-blur absolute w-full h-full"></div>
-    <div
-      class="as-main-link w-full justify-between md:justify-start items-center"
-    >
-      <nuxt-link
-        to="/"
-        class="as-logo-gradient rounded-full py-1.5 px-2 font-body text-lg text-white font-black"
-        >AS</nuxt-link
+  <div class="as-container as-nav-pos fixed  top-0 z-10">
+    <div class=" as-logo-container flex items-center px-4 py-3 ">
+      <div class="as-blur absolute w-full h-full"></div>
+      <div
+        class="as-main-link w-full justify-between md:justify-start items-center"
       >
-      <a class="text-gray-300 ml-6 hover:text-white " v-scroll-to="'#skills'"
-        >Skills</a
-      >
-      <a class="text-gray-300 ml-4 hover:text-white " v-scroll-to="'#about'"
-        >About</a
-      >
-      <a
-        class="text-gray-300 flex ml-4 py-2 px-4 rounded-full bg-green-200 hover:text-white "
-        style="background:#ffffff0d"
-        href="mailto:hi@afaraseun.com"
-        >Contact <span class="ml-1 hidden lg:block">Me</span></a
-      >
+        <nuxt-link
+          to="/"
+          class="as-logo-gradient rounded-full py-1.5 px-2 font-body text-lg text-white font-black"
+          >AS</nuxt-link
+        >
+        <a class="text-gray-300 ml-6 hover:text-white " v-scroll-to="'#skills'"
+          >Skills</a
+        >
+        <a class="text-gray-300 ml-4 hover:text-white " v-scroll-to="'#about'"
+          >About</a
+        >
+        <a
+          class="text-gray-300 flex ml-4 py-2 px-4 rounded-full bg-green-200 hover:text-white "
+          style="background:#ffffff0d"
+          href="mailto:hi@afaraseun.com"
+          >Contact <span class="ml-1 hidden lg:block">Me</span></a
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -43,5 +43,17 @@
   left: 0;
   right: 0;
   bottom: 0;
+}
+
+.as-nav-pos {
+  left: 0;
+  right: 0;
+}
+
+@media (min-width: 768px) {
+  .as-nav-pos {
+    left: unset;
+    right: unset;
+  }
 }
 </style>
