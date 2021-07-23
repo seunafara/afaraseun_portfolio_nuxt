9d3400d78537dc1149eaae734d3d67f1b391ccb5
Afara Seun <afaraseun@Afaras-MacBook-Pro.local>
2021-07-23 15:59:43 -0500

    fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,16 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "afaraseun_nuxt",
+    title: "afaraseun_portfolio",
     htmlAttrs: {
-      lang: "en"
+      lang: "en",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -20,7 +20,7 @@ export default {
   plugins: [
     { src: "~/plugins/aos", ssr: false },
     { src: "~/plugins/lazyload" },
-    { src: "~/plugins/underscore", ssr: false }
+    { src: "~/plugins/underscore", ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -29,12 +29,12 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss"
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [["vue-scrollto/nuxt", { duration: 250 }]],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {},
 };
